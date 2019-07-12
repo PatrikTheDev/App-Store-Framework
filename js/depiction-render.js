cc0672f0a87e91329a1fc6076e1cda927c9347cb
Patrik Svoboda <svob.pata@gmail.com>
2019-07-12 12:16:15 +0200

    Minor change
    Added a check for appending the depiction views (if it doesn't match any supported classes, it will throw an error)

--- js/depiction-render.js
+++ js/depiction-render.js
@@ -15,9 +15,13 @@ function appendDescription(path, element, JSONData) {
         }
 
         var i;
+        var success;
         // Loop through views and append them
         for (i = 0; i < JSONData.description.length; i++) {
-            depictionAppendEngine(element, JSONData, i);
+            success = depictionAppendEngine(element, JSONData, i);
+            if (success === false) {
+                console.error("unsupported class");
+            }
         }
     }
     element.attr("alreadyRan", "true");
@@ -33,12 +37,12 @@ function depictionAppendEngine(element, JSONData, index) {
     // Header
     if (currentClass == "DepictionHeaderView") {
         element.append("<h1>" + currentView.title + "</h1>");
-        return;
+        return true;
     }
     // Subheader
     if (currentClass == "DepictionSubheaderView") {
         element.append("<h2>" + currentView.title + "</h2>");
-        return;
+        return true;
     }
     // Markdown
     if (currentClass == "DepictionMarkdownView") {
@@ -67,28 +71,29 @@ function depictionAppendEngine(element, JSONData, index) {
             $("[spawnedId=" + index + "]").css("color", currentView.textColor);
         }
         $("[spawnedId=" + index + "]").removeAttr("spawnedId");
-        return;
+        return true;
     }
     // Video
     if (currentClass == "DepictionVideoView") {
         element.append(video(currentView.URL, borderRadius));
-        return;
+        return true;
     }
     // Image
     if (currentClass == "DepictionImageView") {
         element.append(image(currentView.URL, currentView.width, currentView.height, borderRadius));
-        return;
+        return true;
     }
     // Spacer
     if (currentClass == "DepictionSpacerView") {
         element.append(spacer(spacing));
-        return;
+        return true;
     }
     // Separator
     if (currentClass == "DepictionSeparatorView") {
         element.append(separator());
-        return;
+        return true;
     }
+    return false;
 }
 function renderMarkdown(markdown) {
     var md = window.markdownit();
