f0d4f952f2eb87a5e7e807a77a89e582a41b8295
Patrik Svoboda <svob.pata@gmail.com>
2019-09-17 20:58:05 +0200

    depiction rendering partial rewrite

--- js/depiction-render.js
+++ js/depiction-render.js
@@ -33,52 +33,48 @@ function depictionAppendEngine(element, JSONData, index) {
         markdown,
         useRawFormat,
         markdownHTML;
-    // Header
-    if (currentClass === "DepictionHeaderView") {
-        element.append(`<h1>${currentView.title}</h1>`);
-        return true;
+
+    switch (currentClass) {
+        // Header
+        case "DepictionHeaderView":
+            element.append(`<h1>${currentView.title}</h1>`);
+            return true;
+        // Subheader
+        case "DepictionSubheaderView":
+            element.append(`<h2>${currentView.title}</h2>`);
+            return true;
+        // Label
+        case "DepictionLabelView":
+            element.append(label(currentView));
+            return true;
+        // Markdown
+        case "DepictionMarkdownView":
+            // Setup markdown
+            markdown = currentView.markdown;
+            useRawFormat = currentView.useRawFormat || false;
+            markdownHTML = useRawFormat ? currentView.markdown : renderMarkdown(markdown);
+            // Append it
+            element.append(markdownHTML);
+            return true;
+        // Video
+        case "DepictionVideoView": 
+            element.append(video(currentView));
+            return true;
+        // Image
+        case "DepictionImageView":
+            element.append(image(currentView));
+            return true;
+        // Spacer
+        case "DepictionSpacerView":
+            element.append(spacer(currentView.spacing));
+            return true;
+        // Separator
+        case "DepictionSeparatorView":
+            element.append(separator(currentView));
+            return true;
+        default:
+            return false
     }
-    // Subheader
-    if (currentClass === "DepictionSubheaderView") {
-        element.append(`<h2>${currentView.title}</h2>`);
-        return true;
-    }
-    // Markdown
-    if (currentClass === "DepictionMarkdownView") {
-        // Setup markdown
-        markdown = currentView.markdown;
-        useRawFormat = currentView.useRawFormat || false;
-        markdownHTML = useRawFormat ? currentView.markdown : renderMarkdown(markdown);
-        // Append it
-        element.append(markdownHTML);
-        return true;
-    }
-    // Label
-    if (currentClass === "DepictionLabelView") {
-        element.append(label(currentView));
-        return true;
-    }
-    // Video
-    if (currentClass === "DepictionVideoView") {
-        element.append(video(currentView));
-        return true;
-    }
-    // Image
-    if (currentClass === "DepictionImageView") {
-        element.append(image(currentView));
-        return true;
-    }
-    // Spacer
-    if (currentClass === "DepictionSpacerView") {
-        element.append(spacer(currentView.spacing));
-        return true;
-    }
-    // Separator
-    if (currentClass === "DepictionSeparatorView") {
-        element.append(separator(currentView));
-        return true;
-    }
-    return false;
 }
 function renderMarkdown(markdown) {
     var md = window.markdownit(),
